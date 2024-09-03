b472d0321c9b23caefa6826411083bb223c10b31
raphaeldev01 <raphael.developer@hotmail.com>
2024-09-03 18:52:09 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicio:
+            double num1, num2, res = 0;
+            char op;
+
+            Console.WriteLine("=====================================");
+            Console.Write("Numero 1: ");
+            num1 = double.Parse(Console.ReadLine());
+            Operacao:
+            Console.Write("Operação(+ - * /): ");
+            op = char.Parse(Console.ReadLine());
+            Console.Write("Numero 2: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            switch(op)
+            {
+                default:
+                    Console.WriteLine("Coloca o bagulho certo filho da puta");
+                    goto Inicio;
+                case '+':
+                    res = num1 + num2;
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    break;
+                case '/':
+                    res = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine("=====================================");
+            Console.WriteLine(num1 + " " + op + " " + num2 + " = " + res);
+
+            num1 = res;
+            Console.WriteLine("=====================================");
+            goto Operacao;
+
+        }
+    }
+}
